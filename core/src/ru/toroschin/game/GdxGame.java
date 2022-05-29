d58351a900ac1d2944a20871e84f3ee28531e9a7
Aleksandr-Toroschin <a_toroschin@mail.ru>
2022-05-29 12:50:44 +0300

    app for lesson 1

--- core/src/ru/toroschin/game/GdxGame.java
+++ core/src/ru/toroschin/game/GdxGame.java
@@ -1,6 +1,8 @@
 package ru.toroschin.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,7 +10,8 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class GdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	float time;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -17,10 +20,20 @@ public class GdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.1f, 0.1f, 0.3f, 1);
+
+		float x = Gdx.input.getX();
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+		time += Gdx.graphics.getDeltaTime();
+		System.out.println(time);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, x, y, 0, 0, img.getWidth(), img.getHeight(), 1, 1, 0, 0, 0, img.getWidth(), img.getHeight(), false, false);
 		batch.end();
+		if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+			ScreenUtils.clear(0.1f, 0.1f, 0.3f, 1);
+		}
 	}
 	
 	@Override
