7db6041361456561db7fca91e7007b2bb60f5461
Aleksandr-Toroschin <a_toroschin@mail.ru>
2022-06-07 22:10:41 +0300

    lesson 3

--- core/src/ru/toroschin/game/Coin.java
+++ core/src/ru/toroschin/game/Coin.java
@@ -1,5 +1,7 @@
 package ru.toroschin.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -9,12 +11,17 @@ public class Coin {
     private Vector2 position;
 
     public Coin(Vector2 position) {
-        this.player = new AnimationPLayer("Full Coins.png", 8, 1, 10, Animation.PlayMode.LOOP);
+
+        this.player = new AnimationPLayer("maps/Full_coins.png", 8, 1, 10, Animation.PlayMode.LOOP);
         this.position = position;
     }
 
-    public void draw(SpriteBatch batch) {
-        batch.draw(player.getFrame(), position.x, position.y);
+    public void draw(SpriteBatch batch, OrthographicCamera camera) {
+        player.step(Gdx.graphics.getDeltaTime());
+        float cx = (position.x - camera.position.x)/camera.zoom + Gdx.graphics.getWidth()/2;
+        float cy = (position.y - camera.position.y)/camera.zoom + Gdx.graphics.getHeight()/2;
+
+        batch.draw(player.getFrame(), cx, cy);
     }
 
     public void dispose() {
--- core/src/ru/toroschin/game/GdxGame.java
+++ core/src/ru/toroschin/game/GdxGame.java
@@ -7,60 +7,78 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GdxGame extends ApplicationAdapter {
-    SpriteBatch batch;
-//    Texture img;
-    TextureRegion region;
-    float time;
-    float scale;
-    AnimationPLayer animationMario;
-    Label label;
+    private SpriteBatch batch;
+    private TextureRegion region;
+    private float time;
+    private float scale;
+    private AnimationPLayer animationMario;
+    private Label label;
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
     private OrthographicCamera camera;
+    private List<Coin> coins;
 
+    private int[] foreGround, backGround;
     private int x, y;
 
     @Override
     public void create() {
+//        coins.add(new Coin(new Vector2(0,0)));
+
         batch = new SpriteBatch();
-//        img = new Texture("Header.png");
         scale = 1;
-//        region = new TextureRegion(img);
         animationMario = new AnimationPLayer("mario.png", 9, 1, 16, Animation.PlayMode.LOOP);
 
         label = new Label(36);
-        map = new TmxMapLoader().load("maps/map1.tmx");
+        map = new TmxMapLoader(). load("maps/map2.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+        foreGround = new int[1];
+        foreGround[0] = map.getLayers().getIndex("Tiles2");
+        backGround = new int[1];
+        backGround[0] = map.getLayers().getIndex("Tiles1");
+
+
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.update();
-        RectangleMapObject o = (RectangleMapObject) map.getLayers().get("Слой объектов 1").getObjects().get("camera");
+        RectangleMapObject o = (RectangleMapObject) map.getLayers().get("camera").getObjects().get("camera");
         camera.position.x = o.getRectangle().x;
         camera.position.y = o.getRectangle().y;
+        camera.zoom = 0.7f;
         camera.update();
+
+        coins = new ArrayList<>();
+        MapLayer mapLayerCoin = map.getLayers().get("Coins");
+        if (mapLayerCoin != null) {
+            MapObjects mo = mapLayerCoin.getObjects();
+            if (mo.getCount() > 0) {
+                for (MapObject mapObject : mo) {
+                    RectangleMapObject rect = (RectangleMapObject) mapObject;
+                    coins.add(new Coin(new Vector2(rect.getRectangle().x, rect.getRectangle().y)));
+                }
+            }
+
+        }
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.1f, 0.1f, 0.3f, 1);
-
-//        float x = Gdx.input.getX();
-//        float y = Gdx.graphics.getHeight() - Gdx.input.getY();
-
-//        time += Gdx.graphics.getDeltaTime();
-
-//        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-//            scale -= 0.1;
-//        }
-//        if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)) {
-//            scale += 0.1;
-//        }
 
         float prevX = camera.position.x;
         float prevY = camera.position.y;
@@ -71,7 +89,7 @@ public class GdxGame extends ApplicationAdapter {
         camera.update();
 
         mapRenderer.setView(camera);
-        mapRenderer.render();
+        mapRenderer.render(backGround);
 
         if ((Math.abs(prevX - camera.position.x) < 0.01) && (Math.abs(prevY - camera.position.y) < 0.01)) {
             animationMario.reSetTime();
@@ -79,18 +97,22 @@ public class GdxGame extends ApplicationAdapter {
         animationMario.step(Gdx.graphics.getDeltaTime());
 
         batch.begin();
-//        batch.draw(img, 0, 0);
         batch.draw(animationMario.getFrame(), Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-        label.draw(batch, "Привет, мир!", 10, Gdx.graphics.getHeight() - 10);
-//        batch.draw(img, x, y, 0, 0, img.getWidth(), img.getHeight(), scale, scale, 0, 0, 0, img.getWidth(), img.getHeight(), false, false);
+        label.draw(batch, "Марио!", 10, Gdx.graphics.getHeight() - 10);
+        for (Coin coin : coins) {
+            coin.draw(batch, camera);
+        }
         batch.end();
+        mapRenderer.render(foreGround);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-//        img.dispose();
         animationMario.dispose();
         label.dispose();
+        for (Coin coin : coins) {
+            coin.dispose();
+        }
     }
 }
